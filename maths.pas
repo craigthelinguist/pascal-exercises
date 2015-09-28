dff09a232837c595f24ceaff77cdb2e820c63099
craigthelinguist <aaroncraig@protonmail.ch>
2015-09-29 01:07:49 +1300

    Ackermanns, naive binomial, closed form sum.

--- maths.pas
+++ maths.pas
@@ -1,8 +1,49 @@
 
 program Exercises;
 
+type
+   Complex = Record
+      re : Real;
+      im : Real;
+   End;
+
+(* Assignment of real to Complex. *)
+operator := (r : real) z : Complex;
+begin
+   z.re := r;
+   z.im := 0.0;
+end;
+
+(* Assignment of integer to Complex. *)
+operator := (i : Integer) z: Complex;
+begin
+   z.re := i;
+   z.im := 0.0;
+end;
+
+(* Addition of complex w/ complex. *)
+operator + (z1, z2 : Complex) z : Complex;
+begin
+   z.re := z1.re + z2.re;
+   z.im := z1.im + z2.im;
+end;
+
+(* Addition of complex w/ real numbers. *)
+operator + (z1 : Complex; r : Real)  z: Complex;
+begin
+   z.re := z1.re + r;
+   z.im := z.im;
+end;
+
+operator + (r : Real; z1 : Complex) z : Complex;
+begin
+   z.re := z1.re + r;
+   z.im := z.im;
+end;
+
+
 (* Computes the sqrt of the specified integer. Uses Newton's method to
-   apprxoimate to within a certain error.
+   approximate to within a certain error.
       n: number whose sqrt will be computed.
       err: amount of imprecision allowed. *)
 function MySqrt (n : Integer; err : Real) : Real;
@@ -14,6 +55,63 @@ begin
    MySqrt := guess;
 end;
 
+(* An iterative implementation of the factorial function.
+      n : specifies the n'th factorial to compute. *)
+function FactIter (n : Integer) : LongInt;
+var i : Integer;
+begin
+   FactIter := n;
+   for i := n-1 downto 1 do
+      FactIter := FactIter * i;
+end;
+
+(* Recursive implementation of the factorial function.
+      n : specifies n'th factorial to compute. *)
+function FactRec (n : Integer) : LongInt;
+begin
+   if ((n = 0) or (n = 1)) then
+      FactRec := 1
+   else
+      FactRec := n * FactRec(n-1);
+end;
+
+(* Computes n choose k, the binomial choice function.
+   This is the number of ways to choose k-sized subsets from a set of size n. *)
+function BinomialClosed (n : Integer; k : Integer) : LongInt;
+var nFact, kFact : LongInt;
+begin
+   nFact := FactIter(n);
+   kFact := FactIter(k);
+   BinomialClosed := round(nFact/(kFact*(nFact-kFact)));
+end;
+
+(* Computes n choose k using the recursive definition.
+   This is the number of ways to choose k-sized subsets from a set of size n. *)
+function BinomialRec (n : Integer; k : Integer) : LongInt;
+begin
+   if ((k=0) or (k=n)) then
+      BinomialRec := 1
+   else if ((k=1) or (k=n-1)) then
+      BinomialRec := n
+   else
+      BinomialRec := BinomialRec(n-1,k) + BinomialRec(n-1,k-1);
+end;
+
+(* Compute sum of first n computers using a closed-form expression.
+      n : specifies first n numbers to sum. *)
+function ClosedSum (n : Integer) : Integer;
+begin
+   ClosedSum := round(n*(n+1)/2);
+end;
+
+(* Compute sum of the numbers between start and finish.
+      start : start point of numbers
+      finish : end point of numbers. *)
+function ClosedSum (start, finish : Integer) : Integer;
+begin
+   ClosedSum := ClosedSum(finish) - ClosedSum(start);
+end;
+
 (* Iteratively sums the first n numbers.
       n : specifies the first n numbers to sum. *)
 function Sum (n : Integer) : Integer;
@@ -21,19 +119,17 @@ var i : Integer;
 begin
    Sum := 0;
    for i := 1 to n do
-   begin
       Sum := Sum + i;
-   end;
 end;
 
 (* Recursively computes Fibonacci numbers.
       n : specifies the n'th Fibonacci to compute. *)
-function FibRecursive (n : Integer) : Integer;
+function FibRec (n : Integer) : Integer;
 begin
    if ((n=1) or (n=0)) then
-      FibRecursive := 1
+      FibRec := 1
    else
-      FibRecursive := FibRecursive(n-1) + FibRecursive(n-2)
+      FibRec := FibRec(n-1) + FibRec(n-2)
 end;
 
 (* Iteratively computes Fibonacci numbers.
@@ -54,17 +150,13 @@ begin
    end;
 end;
 
-(* An iterative implementation of the factorial function.
-      n : specifies the n'th factorial to compute. *)
-function Fact (n : Integer) : LongInt;
-var
-   i : Integer;
+(* Computes the two-variable version of the Ackermann function.
+      m,n : integers to the function. *)
+function Ack (m, n : Integer) : LongInt;
 begin
-   Fact := n;
-   for i := n-1 downto 1 do
-   begin
-      Fact := Fact * i;
-   end;
+   if (m = 0) then Ack := n + 1
+   else if ((m > 0) and (n = 0)) then Ack := Ack(m-1, 1)
+   else Ack := Ack(m-1, Ack(m,n-1));
 end;
 
 begin
